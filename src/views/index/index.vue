0a2ee6513c8227a218f4ba93a9e56fc6ce3f0df3
putongjian <putongjian@outlook.com>
2024-08-20 18:07:06 +0800

    ci: :ferris_wheel: 更新vscode配置

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="text-4">
-      Smiley Sans 得意黑是一款在人文观感和几何特征中寻找平衡的中文黑体
-      <n-button @click="onClick">Click</n-button>
-    </div>
+  <div class="text-4">
+    Smiley Sans 得意黑是一款在人文观感和几何特征中寻找平衡的中文黑体
+    <n-button @click="onClick">Click</n-button>
+  </div>
 </template>
 
 <script setup lang="ts">
