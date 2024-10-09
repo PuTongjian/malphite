a7719e9f1a9d143a4e8124188b6eb3e58efb210e
putongjian <putongjian@outlook.com>
2024-10-09 18:08:42 +0800

    refactor: :recycle: 修改项目构建相关代码

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+declare namespace Env {
+  /** Interface for import.meta */
+  interface CustomImportMetaEnv extends ImportMetaEnv {
+    /** The base url of the application */
+    readonly VITE_BASE_URL: string
+    /** The base url of the backend service */
+    readonly VITE_SERVICE_BASE_URL: string
+  }
+}
+
+interface ImportMeta {
+  env: Env.CustomImportMetaEnv
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,46 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { resolve } from "node:path";
+import process from "node:process";
 import Vue from "@vitejs/plugin-vue";
 import UnoCSS from "unocss/vite";
-import { resolve } from "node:path";
 import Components from "unplugin-vue-components/vite";
 import ElegantVueRouter from "@elegant-router/vue/vite";
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    Vue(),
+export default defineConfig(configEnv => {
+  const viteEnv =  loadEnv(configEnv.mode, process.cwd()) as unknown as Env.CustomImportMetaEnv;
 
-    ElegantVueRouter({
+  return {
+    base: viteEnv.VITE_BASE_URL,
+    plugins: [
+      Vue(),
+
+      ElegantVueRouter({
+        alias: {
+          "@": "src",
+        },
+        layouts: {
+          basic: "src/layout/basic-layout/index.vue",
+        },
+        dtsDir: "typings/elegant-router.d.ts"
+      }),
+
+      Components({
+        extensions: ["vue"],
+        dts: "typings/components.d.ts",
+        include: [/\.vue$/, /\.vue\?vue/],
+        exclude: [/node_modules/],
+        dirs: ["src/components"],
+      }),
+
+      UnoCSS()
+    ],
+    resolve: {
       alias: {
-        "@": "src",
-      },
-      layouts: {
-        basic: "src/layout/basic-layout/index.vue",
-      },
-    }),
-
-    Components({
-      extensions: ["vue"],
-      dts: "typings/components.d.ts",
-      include: [/\.vue$/, /\.vue\?vue/],
-      exclude: [/node_modules/],
-      dirs: ["src/components"],
-    }),
-
-    UnoCSS()
-  ],
-  resolve: {
-    alias: {
-      "~": resolve(__dirname),
-      "@": resolve(__dirname, "src")
+        "~": resolve(__dirname),
+        "@": resolve(__dirname, "src")
+      }
     }
-  }
+  };
 });
