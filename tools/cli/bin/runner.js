72be7d56e925fb42a19d9a7a27c6dfe9a2fd147c
putongjian <putongjian@outlook.com>
2025-12-25 23:00:53 +0800

    feat: 添加配置文件和CLI工具，支持文件运行和格式化设置

--- tools/cli/bin/runner.js
+++ tools/cli/bin/runner.js
@@ -1,3 +1,58 @@
-console.log('This is the runner script for Malphite CLI');
+import { fileURLToPath } from 'node:url'
+import { join } from 'node:path'
+import { existsSync } from 'node:fs'
 
-console.log(import.meta.url);
+
+const scriptsFolder = join(fileURLToPath(import.meta.url), '..', '..')
+const scriptSrcFolder = join(scriptsFolder, 'src')
+const projectRoot = join(scriptsFolder, '..', '..')
+
+const [node, _self, file] = process.argv
+
+
+if (!file) {
+  console.error(`Please provide a file to run, e.g. 'run src/index.{js/ts}'`)
+  process.exit(1)
+}
+
+const fileLocationCandidates = new Set([
+  process.cwd(),
+  scriptSrcFolder,
+  projectRoot,
+])
+const lookups = []
+
+/**
+ * @type {string | undefined}
+ */
+let scriptLocation
+
+for (const location of fileLocationCandidates) {
+  if (scriptLocation) {
+    break
+  }
+
+  const fileCandidates = [file, `${file}.js`, `${file}.ts`]
+  for (const candidates of fileCandidates) {
+    const candidateLocation = join(location, candidates)
+    if (existsSync(candidateLocation)) {
+      scriptLocation = candidateLocation
+      break
+    }
+    lookups.push(candidateLocation)
+  }
+}
+
+if (!scriptLocation) {
+  console.error(`File ${file} not found, please make sure the first parameter passed to 'run' script is a valid js or ts file.`)
+  console.error(`Searched locations: `)
+  lookups.forEach(location => {
+    console.error(`  - ${location}`)
+  })
+  process.exit(1)
+}
+
+// const nodeOptions = []
+
+// if (scriptLocation.endsWith('.ts') || scriptLocation)
+var spawn = require('child_process').spawn   
