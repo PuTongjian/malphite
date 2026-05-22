c5618d4f0bf5c8c161bfdb81a5a53412f752bb2b
putongjian <putongjian@outlook.com>
2026-05-22 18:00:30 +0800

    feat: implement bundling command and initial frontend setup with HTML target configuration

---
+++ tools/cli/src/bundle.ts
@@ -0,0 +1,33 @@
+import type { Package } from "@malphite-tools/utils/workspace";
+import { Option } from "clipanion";
+// import { createServer, mergeConfig, type UserConfig } from "vite";
+import { PackageCommand } from "./command";
+import { ceateHTMLTargetConfig } from "./vite/index";
+
+export class BundleCommand extends PackageCommand {
+  static override paths = [["bundle"], ["pack"], ["bun"]];
+
+  override _deps = false;
+  override waitDeps = false;
+
+  dev = Option.Boolean("--dev,-d", false, {
+    description: "Run in Development mode",
+  });
+
+  async execute() {
+    const pkg = this.workspace.getPackage(this.package);
+
+    if (this.dev) {
+      await BundleCommand.dev(pkg);
+    }
+  }
+
+  static async dev(pkg: Package) {
+    process.env.NODE_ENV = "development";
+
+    const server = await createServer(ceateHTMLTargetConfig(pkg));
+
+    await server.listen();
+    server.printUrls();
+  }
+}
--- tools/cli/src/vite/index.ts
+++ tools/cli/src/vite/index.ts
@@ -10,5 +10,53 @@ export function ceateHTMLTargetConfig(pkg: Package): UserConfig {
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
       __PACKAGE_NAME__: JSON.stringify(pkg.name),
     },
+    resolve: {
+      alias: {
+        // "@malphite/core": ProjectRoot.join("packages/frontend/core/src").value,
+      },
+      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
+    },
+    build: {
+      outDir: pkg.join("dist").value,
+      emptyOutDir: false,
+      sourcemap: true,
+      target: "es2022",
+      manifest: true,
+      rolldownOptions: {
+        input: pkg.join("index.html").value,
+        output: {
+          entryFileNames: "js/[name].[hash].js",
+          chunkFileNames: "js/[name].[hash].js",
+          assetFileNames: "assets/[name].[hash][extname]",
+          codeSplitting: {
+            groups: [
+              {
+                name: "vendor",
+                test: /[/\\]node_modules[/\\]/,
+              },
+            ],
+          },
+        },
+      },
+    },
+    server: {
+      host: "0.0.0.0",
+      strictPort: false,
+      // proxy: {
+      //   "/api": {
+      //     target: "http://localhost:3010",
+      //     changeOrigin: true,
+      //   },
+      //   "/graphql": {
+      //     target: "http://localhost:3010",
+      //     changeOrigin: true,
+      //   },
+      //   "/socket.io": {
+      //     target: "http://localhost:3010",
+      //     ws: true,
+      //     changeOrigin: true,
+      //   },
+      // },
+    },
   };
 }
